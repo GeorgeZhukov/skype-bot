69f9c8e39870cb60b47e6a31184a8f40054b3bce
George Zhukov <scofield.cross@gmail.com>
2015-11-04 23:41:40 +0200

    added feature "network sync"

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 gem 'terminal-table'
 gem 'skype'
 gem 'ruby-dbus' if RUBY_PLATFORM =~ /linux/i
+gem 'slop'
--- main.rb
+++ main.rb
@@ -2,11 +2,63 @@ require 'rubygems'
 require 'terminal-table'
 require 'yaml'
 require 'skype'
+require 'slop'
+require 'socket'
+
+class Sender
+  NOTIFY_MSG = "victim detected!"
+
+  def initialize(port)
+    @port = port
+    @sock = UDPSocket.new()
+    @sock.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+
+    ObjectSpace.define_finalizer(self, proc { @sock.close })
+
+  end
+
+  def notify()
+    @sock.send(NOTIFY_MSG, 0, "<broadcast>", @port)
+  end
+
+end
+
+class Receiver
+  attr_accessor :callback
+
+  def initialize(port)
+    BasicSocket.do_not_reverse_lookup = true
+
+    @port = port
+    @sock = UDPSocket.new
+    @sock.bind("0.0.0.0", @port)
+
+    ObjectSpace.define_finalizer(self, proc { @sock.close })
+  end
+
+  def process()
+    while true
+      data, addr = @sock.recvfrom(1024)
+
+      @callback.call(data)
+    end
+  end
+end
+
+
 
 # RubyGarage Skype Bot
 class SkypeBot
   def initialize
     @config = YAML.load_file('config.yml')
+    @opts = parse_opts()
+
+    if @opts.sync?
+      init_receiver()
+      init_sender()
+
+      puts 'Sync activated!'
+    end
 
     Skype.config app_name: 'RubyGarageBot'
   end
@@ -35,6 +87,30 @@ class SkypeBot
 
   private
 
+  def parse_opts
+    Slop.parse do |o|
+      o.bool '-s', '--sync', 'synchronize bots over network'
+    end
+  end
+
+  def init_sender
+    @sender = Sender.new(@config["port"])
+  end
+
+  def init_receiver
+    @receiver = Receiver.new(@config["port"])
+    @receiver.callback = method(:sync)
+
+    Thread.new { @receiver.process() }
+  end
+
+  # this methods called when someone broadcast
+  # notify message over network
+  def sync(data)
+    send_message()
+    @handled = true
+  end
+
   def watcher
     last_message_id = 0
 
@@ -52,12 +128,19 @@ class SkypeBot
   end
 
   def process(message)
+    return @handled = false if @handled
+
     if valid?(message)
-      @chat.post @config['message']
-      print '*'
+      @sender.notify() if @sender
+      send_message()
     else
       print '.'
     end
+  end
+
+  def send_message
+    @chat.post @config['message']
+    print '*'
   end
 
   def valid?(message)
